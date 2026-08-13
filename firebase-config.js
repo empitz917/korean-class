2693591c983a870fd82c9ce35abfac48f6432f40
SHJ <bapul917@gmail.com>
2026-08-13 17:35:03 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// koara 프로젝트 Firebase 설정값
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeRbCCqSBYo1eUHIpcGamH-bNAQz0__rc",
+  authDomain: "koara-f7b52.firebaseapp.com",
+  projectId: "koara-f7b52",
+  storageBucket: "koara-f7b52.firebasestorage.app",
+  messagingSenderId: "941168145118",
+  appId: "1:941168145118:web:718aa1b781eabc0ae9884f",
+  measurementId: "G-375P0F1EVY"
+};
